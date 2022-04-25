71035a8f16b055e46cc070546c3388f2555f703b
Bouraoui zich <38636485+sag3ellil@users.noreply.github.com>
2022-04-25 03:45:15 +0100

    Identation and removing the unnecessary lines of code

--- src/main/java/com/bank/NewAccount/NewAccountApplication.java
+++ src/main/java/com/bank/NewAccount/NewAccountApplication.java
@@ -16,20 +16,14 @@ public class NewAccountApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NewAccountApplication.class, args);
 	}
-	
+
 	@Bean
-	CommandLineRunner runner(CostumerRepository costumerRepo,AccountRepository accountRepo)
-	{
+	CommandLineRunner runner(CostumerRepository costumerRepo, AccountRepository accountRepo) {
 		return args -> {
-			Costumer c = new Costumer("Bouraoui","Ziche");
+			//We create this user to have an existed user for our example  
+			Costumer c = new Costumer("Bouraoui", "Ziche");
 			costumerRepo.save(c);
-			
-			Double amount = 20.00;
-			Account account = new Account(amount,c);
-			accountRepo.save(account);
-			
-			
-			//System.out.println(costumerRepo.findAll());
+
 		};
 	}
 
