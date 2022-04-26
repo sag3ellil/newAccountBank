cdc92739858a659271737377eb4acdfa1b5be049
Bouraoui zich <38636485+sag3ellil@users.noreply.github.com>
2022-04-26 19:23:18 +0100

    Correcting the date formate

--- src/main/java/com/bank/NewAccount/Services/TransactionTrackService.java
+++ src/main/java/com/bank/NewAccount/Services/TransactionTrackService.java
@@ -1,5 +1,6 @@
 package com.bank.NewAccount.Services;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.springframework.stereotype.Service;
@@ -17,24 +18,23 @@ public class TransactionTrackService {
 	private final TransactionRepository transactionRepo;
 	private final MessageUtil messageUtil;
 	private final TransactionConverter transactionConv;
-	
-	public TransactionTrackService(TransactionRepository transactionRepo, MessageUtil messageUtil,TransactionConverter transactionConv)
-	{
-		this.messageUtil=messageUtil;
-		this.transactionRepo=transactionRepo;
-		this.transactionConv=transactionConv;
+
+	public TransactionTrackService(TransactionRepository transactionRepo, MessageUtil messageUtil,
+			TransactionConverter transactionConv) {
+		this.messageUtil = messageUtil;
+		this.transactionRepo = transactionRepo;
+		this.transactionConv = transactionConv;
 	}
-	
-	
-	public TransactionView saveTransaction(Account account,Double amount)
-	{  
-       Transaction transaction = new Transaction(amount, new Date(), account);
-       Transaction transToReturn = this.transactionRepo.save(transaction);
-       return this.convertToView(transToReturn);
+
+	public TransactionView saveTransaction(Account account, Double amount) {
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date date = new Date();
+		Transaction transaction = new Transaction(amount, formatter.format(date), account);
+		Transaction transToReturn = this.transactionRepo.save(transaction);
+		return this.convertToView(transToReturn);
 	}
-	
-	public TransactionView convertToView(Transaction t)
-	{
+
+	public TransactionView convertToView(Transaction t) {
 		return this.transactionConv.convert(t);
 	}
 }
